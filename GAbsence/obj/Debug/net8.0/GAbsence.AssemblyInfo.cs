293ca184aa655a4712f8db505d04715d6cfa7c32
Amen Allah Jouila <amenallah1991@gmail.com>
2025-01-16 14:04:01 +0100

    adding dashboard for etudiants and enseignants

--- GAbsence/obj/Debug/net8.0/GAbsence.AssemblyInfo.cs
+++ GAbsence/obj/Debug/net8.0/GAbsence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GAbsence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd57961feba28e7ddd22237a3a14ea454687d413")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8fc742ec07608e59358dda03b4af22f162e2a3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GAbsence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GAbsence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
